b54970fc2679105119ffb1a2f5bbfc7dd6dc3ea4
frk1 <11559382+frk1@users.noreply.github.com>
2021-11-04 23:51:50 +0100

    Update mpvgit.rb

--- mpvgit.rb
+++ mpvgit.rb
@@ -1,59 +1,92 @@
-# typed: false
-# frozen_string_literal: true
-
 class Mpvgit < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
-  url "https://github.com/mpv-player/mpv/archive/6e6ec331685c78584a818f524286670911e8b4af.tar.gz"
-  version "0.29.1~6e6ec33"
-  sha256 "fbb5ebc72c55af6e62cb3835b87b0fd26160533350f17e73712791870bdbe017"
-  head "https://github.com/mpv-player/mpv.git"
+  url "https://github.com/mpv-player/mpv/archive/v0.34.0.tar.gz"
+  sha256 "f654fb6275e5178f57e055d20918d7d34e19949bc98ebbf4a7371902e88ce309"
+  license :cannot_represent
+  head "https://github.com/mpv-player/mpv.git", branch: "master"
 
   depends_on "docutils" => :build
   depends_on "pkg-config" => :build
-  depends_on "python" => :build
+  depends_on "python@3.10" => :build
+  depends_on xcode: :build
 
   depends_on "ffmpeg"
+  depends_on "libass"
   depends_on "jpeg"
   depends_on "libarchive"
-  depends_on "libass"
   depends_on "little-cms2"
-  depends_on "lua@5.1"
-
+  depends_on "luajit-openresty"
   depends_on "mujs"
+  depends_on "rubberband"
   depends_on "uchardet"
   depends_on "vapoursynth"
-  depends_on "youtube-dl"
+  depends_on "zimg"
+
+  depends_on "youtube-dl" => :recommended
+  depends_on "subliminal" => :recommended
+
+  depends_on "jack" => :optional
+  depends_on "libaacs" => :optional
+  depends_on "libbluray" => :optional
+  depends_on "libcaca" => :optional
+  depends_on "libcdio" => :optional
+  depends_on "libdvdnav" => :optional
+  depends_on "libdvdread" => :optional
+  depends_on "sdl2" => :optional
+
+  on_macos do
+    depends_on "coreutils" => :recommended
+    depends_on "dockutil" => :recommended
+    depends_on "tag" => :recommended
+    depends_on "trash" => :recommended
+  end
 
   def install
+    opts  = Hardware::CPU.arm? ? "-mcpu=native " : "-march=native -mtune=native "
+    opts += "-Ofast -flto=thin -funroll-loops -fomit-frame-pointer "
+    opts += "-ffunction-sections -fdata-sections -fstrict-vtable-pointers -fwhole-program-vtables "
+    opts += "-fforce-emit-vtables " if MacOS.version >= :mojave
+    ENV.append "CFLAGS",      opts
+    ENV.append "CPPFLAGS",    opts
+    ENV.append "CXXFLAGS",    opts
+    ENV.append "OBJCFLAGS",   opts
+    ENV.append "OBJCXXFLAGS", opts
+    ENV.append "LDFLAGS",     opts + " -dead_strip"
+
     # LANG is unset by default on macOS and causes issues when calling getlocale
     # or getdefaultlocale in docutils. Force the default c/posix locale since
     # that's good enough for building the manpage.
-    ENV["LC_ALL"] = "C"
+    ENV["LC_ALL"] = "en_US.UTF-8"
+    ENV["LANG"]   = "en_US.UTF-8"
+
+    # libarchive is keg-only
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
+    # luajit-openresty is keg-only
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["luajit-openresty"].opt_lib/"pkgconfig"
 
     args = %W[
       --prefix=#{prefix}
-      --enable-html-build
-      --enable-javascript
-      --enable-libmpv-shared
-      --enable-lua
-      --enable-libarchive
-      --enable-uchardet
       --confdir=#{etc}/mpv
       --datadir=#{pkgshare}
-      --mandir=#{man}
       --docdir=#{doc}
+      --mandir=#{man}
+      --zshdir=#{zsh_completion}
+
+      --disable-html-build
+      --enable-libmpv-shared
     ]
+    args << "--swift-flags=-O -wmo"
 
-    system "./bootstrap.py"
-    system "python3", "waf", "configure", *args
-    system "python3", "waf", "install"
-
-    system "python3", "TOOLS/osxbundle.py", "build/mpv"
+    system Formula["python@3.10"].opt_bin/"python3", "bootstrap.py"
+    system Formula["python@3.10"].opt_bin/"python3", "waf", "configure", *args
+    system Formula["python@3.10"].opt_bin/"python3", "waf", "install"
+    system Formula["python@3.10"].opt_bin/"python3", "TOOLS/osxbundle.py", "build/mpv"
     prefix.install "build/mpv.app"
   end
 
   test do
     system bin/"mpv", "--ao=null", test_fixtures("test.wav")
+    assert_match "vapoursynth", shell_output(bin/"mpv --vf=help")
   end
 end
